8ae041017035588c954dc4f9e7909dee72456b1b
evanhurleyodwyer <hurleyoe@tcd.ie>
2022-12-19 22:11:22 +0000

    A beginning - moving images to train/val/test folders

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# This is a sample Python script.
+#!pip install split-folders
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+import pandas as pd
+import os
+import keras
+import matplotlib.pyplot as plt
+import shutil
+import splitfolders
 
+os.mkdir('/kaggle/working/main_directory')
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+kaggle_input_path = '/kaggle/input/covid19-radiography-database/COVID-19_Radiography_Dataset/'
 
+#Copy files to one folder
+shutil.copytree(kaggle_input_path + 'Normal/images','/kaggle/working/main_directory/Normal')
+shutil.copytree(kaggle_input_path+ 'COVID/images','/kaggle/working/main_directory/COVID')
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+#split into train/val/test splits
+splitfolders.ratio('/kaggle/working/main_directory/', '/kaggle/working/output',seed=1337,ratio=(0.8,0.1,0.1),move=True)
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
